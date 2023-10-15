4d480f2581c5493e405374f1f73b4b41d1610d71
szekelyzol <szekelyzol@users.noreply.github.com>
2023-10-15 18:34:26 +0000

    Update video-clip schema with descriptions

--- src/model/VideoClip.ts
+++ src/model/VideoClip.ts
@@ -11,8 +11,17 @@
 
 import AttributeType from './AttributeType.js';
 
+/**
+ * Use this object to create a smaller clip from a video you upload.  - You can only create video clips in the same request where you create the video container. - You cannot update the starting or ending timestamps of a video clip after you created the video container. - When you upload a video file into a container where you defined a starting and ending timestamp, the API trims the video according to those timestamps to create a clip.
+ */
 export default class VideoClip {
+  /**
+   * The timestamp that defines the beginning of the video clip you want to create. The value must follow the `HH:MM:SS` format.
+   */
   'startTimecode'?: string;
+  /**
+   * The timestamp that defines the end of the video clip you want to create. The value must follow the `HH:MM:SS` format.
+   */
   'endTimecode'?: string;
 
   static readonly discriminator?: string = undefined;
